06d07b3384e2c6e5d597337b6b54583b9425378c
Celio Matos <celiomatos@live.com>
2019-02-02 15:38:54 -0400

    super user

---
+++ src/main/java/br/com/dauth/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package br.com.dauth.repository;
+
+import br.com.dauth.model.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuthorityRepository  extends JpaRepository<Authority, Long> {
+}
--- src/main/java/br/com/dauth/service/UserDetailsServiceImpl.java
+++ src/main/java/br/com/dauth/service/UserDetailsServiceImpl.java
@@ -1,5 +1,7 @@
 package br.com.dauth.service;
 
+import br.com.dauth.model.User;
+import br.com.dauth.repository.AuthorityRepository;
 import br.com.dauth.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -13,8 +15,18 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private AuthorityRepository authorityRepository;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return userRepository.findByUsername(username);
+
+        User user = userRepository.findByUsername(username);
+
+        if (null != user && user.isSuperUser()) {
+            user.setAuthorities(authorityRepository.findAll());
+        }
+
+        return user;
     }
 }
